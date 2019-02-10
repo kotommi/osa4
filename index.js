14b1a617d0facc50f405b22b1935ecfb209ccd6c
kotommi <tommi.koivula@helsinki.fi>
2019-02-10 18:49:40 +0200

    added app.js

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 const http = require("http");
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const cors = require("cors");
 const config = require("./utils/config");
 
-app.use(cors());
-app.use(bodyParser.json());
-
-const blogsRouter = require("./ controllers/blogs");
-app.use("/api/blogs", blogsRouter);
+const app = require("./app");
+const server = http.createServer(app);
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
