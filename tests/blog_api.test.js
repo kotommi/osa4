0b27e733db29fcc52100f3a57afd42e93a8f0f60
kotommi <tommi.koivula@helsinki.fi>
2019-02-18 11:23:03 +0200

    refactoring

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,14 +1,27 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
+const helper = require("./test_helper");
 
 const api = supertest(app);
+const Blog = require("../models/blog");
 
-test("blogs are returned as json", async () => {
-  await api
+test("all blogs are returned as json", async () => {
+  const res = await api
     .get("/api/blogs")
     .expect(200)
     .expect("Content-Type", /application\/json/);
+
+  expect(res.body.length).toEqual(helper.initialBlogs.length);
+});
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog);
+    await blogObject.save();
+  }
 });
 
 afterAll(() => {
