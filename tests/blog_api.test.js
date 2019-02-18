f006137dbd93baf4d64deee50ffa3bd3c81473c3
kotommi <tommi.koivula@helsinki.fi>
2019-02-18 11:45:01 +0200

    post test

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -15,6 +15,22 @@ test("all blogs are returned as json", async () => {
   expect(res.body.length).toEqual(helper.initialBlogs.length);
 });
 
+test("post adds one blog to db", async () => {
+  const newBlog = {
+    title: "new blog",
+    author: "tomko",
+    url: "www.github.com",
+    likes: 0
+  };
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201);
+  const blogs = await helper.blogsInDb();
+  expect(blogs.length).toBe(helper.initialBlogs.length + 1);
+  expect(blogs.slice(-1)[0].title).toEqual(newBlog.title);
+});
+
 test("blogs have field id", async () => {
   const res = await api
     .get("/api/blogs")
